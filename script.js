c22d77549168879a4dc7c1aecc6ae6bee39e15a5
01mehran <132054519+01mehran@users.noreply.github.com>
2023-10-05 22:32:52 +0330

    FInal Result

--- script.js
+++ script.js
@@ -7,83 +7,75 @@ inputElement.addEventListener("input", function () {
 
   if (parsedNumber > 20 || parsedNumber < 1) {
     alert("Please enter a number between 1 and 20.");
-    inputElement.value = "";
+    inputElement.value = " ";
+  } else {
+    changePokemon(userInput);
   }
 });
 // END alert message
 // ************
 // START first Sending Request
 
-function changePokemon() {
-  const number = document.getElementById("searchInput").value;
-
+function changePokemon(userInput) {
   axios
-    .get(`https://pokeapi.co/api/v2/pokemon/${number}`)
+    .get(`https://pokeapi.co/api/v2/pokemon/${userInput}`)
     .then((response) => {
       const data = response.data;
       const $ = document;
-      const imgTAg = data.sprites.other['official-artwork'].front_default;
-
+      const imgUrl1 = data.sprites.front_default;
+      const imgUr2 = data.sprites.other["official-artwork"].front_default;
       //  START first part
-      $.getElementById("bulbasaur-word").innerHTML = data.name;
-      $.getElementById("main-dragon").src = imgTAg; 
+
+      $.getElementById("bulbasaur-name").innerHTML = data.name;
+      $.getElementById("First-parent_img").src = imgUrl1;
+
       // END first part
-      $.getElementById("dragon-img_2").src = imgTAg;
-      $.getElementById("bul-word").innerHTML = data.name;
-      $.getElementById("word-above_dragon").innerHTML = data.id;
-      $.getElementById("grass-word").innerHTML = data.types[0].type.name;
+      $.getElementById("Second-parent_img").src = imgUr2;
+      $.getElementById("second-bulbasaur_name").innerHTML = data.name;
+      $.getElementById("dragon-Id").innerHTML = data.id;
+      $.getElementById("pokemone-type").innerHTML = data.types[0].type.name;
       $.getElementById("height").innerHTML = data.height;
       $.getElementById("weight").innerHTML = data.weight;
 
       const formattedId = data.id.toString().padStart(3, "0");
-      $.getElementById("word-above_dragon").innerHTML = formattedId;
+      $.getElementById("dragon-Id").innerHTML = formattedId;
 
-      changePokemon2();
+      changePokemon2(userInput);
     })
     .catch((error) => {
       console.log(error);
     });
 }
 // END first Sending Request
-// *****************
+
 // START second sending Request
 
-function changePokemon2() {
-  const number = document.getElementById("searchInput").value;
+function changePokemon2(userInput) {
   axios
-    .get(`https://pokeapi.co/api/v2/pokemon-species/${number}`)
+    .get(`https://pokeapi.co/api/v2/pokemon-species/${userInput}`)
     .then((response) => {
       const data = response.data;
       const $ = document;
 
       $.getElementById("color").innerHTML = data.color.name;
-      $.querySelector("#gender").innerHTML = data.genera[7].genus;
+      $.querySelector("#pokemon-jender").innerHTML = data.genera[7].genus;
     })
     .catch((error) => {
       console.log(error);
     });
 }
 // START second sending Request
-// START styling 
-
+// START styling
 const linkChange = document.querySelector("#pageLink");
-const theMain = document.getElementById("the-main");
-const main1 = document.querySelector(".main-form");
-const subMain = document.querySelector(".sub-form");
-const main2 = document.getElementById("main2-form");
-
-linkChange.addEventListener("click", function () {
-  if (main2.style.display == "block") {
-    main2.style.display = "none";
+const SecondParent = document.querySelector("#Second-parent");
+// //////////////////////////////////////////////
+linkChange.addEventListener("click", () => {
+  if (SecondParent.style.display == "block") {
+    SecondParent.style.display = "none";
   } else {
-    main2.style.display = "block";
+    SecondParent.style.display = "block";
   }
-
-  theMain.style.display = "flex";
-  theMain.style.justifyContent = "center";
-  theMain.style.columnGap = "50px";
-  main2.style.marginTop = "75px";
-
-  main1.style.margin = "0 auto";
+  // ////////////////////////////////////////////
 });
+
 // END styling
